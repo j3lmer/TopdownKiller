cfdbbacb10442ebd1e1590a7bff1d598ef45a679
j3lmer <jelmer339@gmail.com>
2022-03-19 14:18:40 +0100

    initialise getters & setters for round

--- Assets/Scripts/GameScene/GameController.cs
+++ Assets/Scripts/GameScene/GameController.cs
@@ -15,35 +15,57 @@ namespace GameScene
         private GameObject _player;
         private EnemyController _enemyController;
         
+        private bool[] finishedWaves = {false, false, false};
+
+        public bool[] GetFinishedWaves()
+        {
+            return finishedWaves;
+        }
+
+        public bool GetFinishedWaveAtIndex(int index)
+        {
+            return finishedWaves[index];
+        }
+
+        public void SetFinishedWaves(int index, bool value)
+        {
+            finishedWaves[index] = value;
+        }
+        
         private void Awake()
         {
             _enemyController = gameObject.GetComponent<EnemyController>();
-            _player = MakePlayer();
+            _player          = MakePlayer();
+            
             StartCoroutine(WaveController());
         }
         
         private GameObject MakePlayer() 
         {
-            GameObject player = Instantiate(playerPrefab, new Vector3(), Quaternion.identity);
-            player.AddComponent<Player.Player>();
-            return player;
+            return Instantiate
+                (
+                    playerPrefab,
+                    new Vector3(),
+                    Quaternion.identity
+                )
+                .AddComponent<Player.Player>()
+                .gameObject;
         }
         
         private IEnumerator WaveController()
         {
             //TODO: laat op scherm zien welke wave het is
 
-            bool[] finishedWaves = {false, false, false};
             yield return new WaitForSeconds(1);
             
             Task spawnDefaultEnemies = new Task(WaveComponent(1, _enemyController.SpawnEnemies(), 20));
             
             spawnDefaultEnemies.Finished += delegate(bool manual)
             {
-                finishedWaves[0] = true;
+                SetFinishedWaves(0, true);
             };
 
-            yield return new WaitUntil(() => finishedWaves[0]);
+            yield return new WaitUntil(() => GetFinishedWaveAtIndex(0));
         }
         
         private IEnumerator WaveComponent(int numberOfCouroutines, IEnumerator function, int waveLengthInSeconds)
